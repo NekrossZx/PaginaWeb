857ec06bf14c42f02dcb4e7b894305818ec0db92
NekrossZx <67488626+NekrossZx@users.noreply.github.com>
2022-11-30 19:02:19 -0300

    config

--- api/config.php
+++ api/config.php
@@ -1,11 +1,11 @@
 <?php
 //CONECTAMOS CON LA BBDD
-$conn = oci_connect('ADMINS', '1234', 'localhost');
+$conn = oci_connect('TURISMOREAL', '123', 'localhost');
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }else{
-    echo "<script>console.log('conexion realizada');</script>";
+    echo "conexion realizada";
 }
 
 //oci_close($conn);
